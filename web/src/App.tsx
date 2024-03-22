6a7d02f02b2e9f97db636f63ed24a3fd4030af01
Tomosada Sato <156967539+TomosadaSato@users.noreply.github.com>
2024-03-22 18:00:12 -0400

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import {useRoutes, BrowserRouter as Router} from 'react-router-dom';
+
 import './App.css';
 import Home from './Pages/Home';
 import MyAccount from './Pages/MyAccount';
@@ -6,6 +8,15 @@ import MyOrders from './Pages/MyOrders';
 import NotFound from './Pages/NotFound';
 import SignIn from './Pages/SignIn';
 
+const AppRoutes = () => {
+  let routes = useRoutes( [
+    {path: '/', element: <Home/>},
+    {path: '/my-account', element: <MyAccount/>},
+    {path: '/my-orders', element: <MyOrders/>},
+    {path: '/sign-in', element: <NotFound/>}
+    ]);
+  return routes;
+};
 
 function App() {
   return (
