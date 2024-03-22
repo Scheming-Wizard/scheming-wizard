6f67cd008fca650b3b4a6c2aefc36170ac738118
Tomosada Sato <156967539+TomosadaSato@users.noreply.github.com>
2024-03-22 19:16:31 -0400

    Update index.tsx

--- web/src/Pages/NotFound/index.tsx
+++ web/src/Pages/NotFound/index.tsx
@@ -1,8 +1,12 @@
+import Layout from "../../Components/Layout";
+
 const NotFound: React.FC = () => {
   return (
     <div>
+    <Layout>
           <h1>NotFound</h1>
       {/*Add content here */}
+      </Layout>
     </div>
   );
 };
