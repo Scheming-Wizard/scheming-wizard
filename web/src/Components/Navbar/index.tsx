8dbca07da7d64edc990fb90f7cdf7df1f0f7a16d
Tomosada Sato <156967539+TomosadaSato@users.noreply.github.com>
2024-03-22 18:40:59 -0400

    Create index.tsx

---
+++ web/src/Components/Navbar/index.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const activeStyle = "underline text-gray-500 underline-offset-4";
+
+    return (
+      <nav className="flex justify-between items-center fixed
+          z-10 w-full py-5 px-8 text-md font-light top-0">
+          <ul className="flex items-center gap-3">
+            <li className="font-semibold text-lg">
+              <NavLink to="/">YOUR PROJECT NAME HERE</NavLink>
+            </li>
+            <li>c
+            <NavLink to="/"
+                    className={({ isActive }) => (isActive ? activeStyle : undefined)}>
+                All
+            </NavLink>
+            </li>
+          </ul>
+      </nav>
+
+    )
+}
+
+
+
+export default Navbar;
