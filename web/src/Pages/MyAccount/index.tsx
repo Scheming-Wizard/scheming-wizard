f12371e9f57a2b158a8b4691b0b7d88a301d54fd
Tomosada Sato <156967539+TomosadaSato@users.noreply.github.com>
2024-03-22 19:14:59 -0400

    Update index.tsx

--- web/src/Pages/MyAccount/index.tsx
+++ web/src/Pages/MyAccount/index.tsx
@@ -1,8 +1,12 @@
+import Layout from "../../Components/Layout";
+
 const MyAccount: React.FC = () => {
   return (
     <div>
+    <Layout>
           <h1>MyAccount</h1>
       {/*Add content here */}
+      </Layout>
     </div>
   );
 };
