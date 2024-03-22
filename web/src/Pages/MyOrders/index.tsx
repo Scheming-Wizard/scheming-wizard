9589867540c1b8c7918183d6a1f69ccaf3681fa6
Tomosada Sato <156967539+TomosadaSato@users.noreply.github.com>
2024-03-22 19:16:04 -0400

    Update index.tsx

--- web/src/Pages/MyOrders/index.tsx
+++ web/src/Pages/MyOrders/index.tsx
@@ -1,8 +1,12 @@
+import Layout from "../../Components/Layout";
+
 const MyOrders: React.FC = () => {
   return (
     <div>
+    <Layout>
           <h1>MyOrders</h1>
       {/*Add content here */}
+      </Layout>
     </div>
   );
 };
