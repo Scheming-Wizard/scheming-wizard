ed7302960ec9daef8b9a5dcdd3a4fdb29f01d466
Tomosada Sato <156967539+TomosadaSato@users.noreply.github.com>
2024-03-22 19:16:51 -0400

    Update index.tsx

--- web/src/Pages/SignIn/index.tsx
+++ web/src/Pages/SignIn/index.tsx
@@ -1,8 +1,12 @@
+import Layout from "../../Components/Layout";
+
 const SignIn: React.FC = () => {
   return (
     <div>
+    <Layout>
           <h1>SignIn</h1>
       {/*Add content here */}
+      </Layout>
     </div>
   );
 };
