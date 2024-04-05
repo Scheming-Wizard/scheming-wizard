6e8e19a76138dce70ae857a302e539e1a51b5ed0
Ibrahim Naguthanawala <naguthanawala.2@osu.edu>
2024-04-04 21:00:25 -0400

    catalog

---
+++ src/api/Scheming.Wizard.Domain/Catalog/Item.cs
@@ -0,0 +1,46 @@
+namespace Scheming.Wizard.Domain.Catalog;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Brand { get; set; }
+    public decimal Price { get; set; }
+    public required string Brand { get; set; }
+    public decimal Price { get; set; }
+    public List<Rating> Ratings { get; set; } = new List<Rating>();
+    public Item(string name, string description, string brand, decimal price)
+    {
+        if(string.IsNullOrWhiteSpace(Name))
+        {
+            throw new ArgumentException("name is required")
+        }
+
+        if(string.IsNullOrWhiteSpace(Description))
+        {
+            throw new ArgumentException("description is required")
+        }
+
+        if(string.IsNullOrWhiteSpace(Brand))
+        {
+            throw new ArgumentException("Brand is required")
+        }
+
+        if(Price <= 0)
+        {
+            throw new ArgumentException("Price cannot be 0 or negative")
+        }
+
+        Name = name;
+        Description = description;
+        Brand = brand;
+        Price = price;
+        Id = id;
+    }
+
+    public void AddRating(Rating rating)
+    {
+        Ratings.Add(rating);
+    }
+}
