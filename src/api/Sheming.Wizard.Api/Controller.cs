b7f85b3fb4d5deddbe6116e3673d84a025adfcd8
Ibrahim Naguthanawala <naguthanawala.2@osu.edu>
2024-04-05 13:57:40 -0400

    keep

---
+++ src/api/Sheming.Wizard.Api/Controller.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Scheming.Wizard.Domain.Catalog
+
+namespace Scheming.Wizard.Api.Controllers;
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        
+    }
+}
