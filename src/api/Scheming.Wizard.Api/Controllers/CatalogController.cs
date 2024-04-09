c109fe75128cdde29635701c79898e2da656aa4e
Ibrahim Naguthanawala <naguthanawala.2@osu.edu>
2024-04-09 15:17:11 -0400

    get item by id

--- src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
+++ src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
@@ -12,12 +12,22 @@ namespace Scheming.Wizard.Api.Controllers;
         {
             var items = new[]
             {
-                new Item(name: "Item 1", description: "Description 1", brand: "Brand1", price: 100.00m),
-                new Item(name: "Item 2", description: "Description 2", brand: "Brand2", price: 200.00m),
-                new Item(name: "Item 3", description: "Description 3", brand: "Brand3", price: 300.00m),
-                new Item(name: "Item 4", description: "Description 4", brand: "Brand4", price: 400.00m),
-                new Item(name: "Item 5", description: "Description 5", brand: "Brand5", price: 500.00m),
+                new Item(name: "Item 1", description: "Description 1", brand: "Brand 1", price: 100.00m),
+                new Item(name: "Item 2", description: "Description 2", brand: "Brand 2", price: 200.00m),
+                new Item(name: "Item 3", description: "Description 3", brand: "Brand 3", price: 300.00m),
+                new Item(name: "Item 4", description: "Description 4", brand: "Brand 4", price: 400.00m),
+                new Item(name: "Item 5", description: "Description 5", brand: "Brand 5", price: 500.00m)
             };
             return Ok(items);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Item 1", "Description 1", "Brand 1", 100.00m);
+            {
+                item.Id = id;
+            }
+            return Ok(item);
         }
     }
