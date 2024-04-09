1166f263089cf80e3603c62fa6efa5613348f0ab
Ibrahim Naguthanawala <naguthanawala.2@osu.edu>
2024-04-09 14:47:17 -0400

    get items and swagger

--- src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
+++ src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
@@ -10,6 +10,14 @@ namespace Scheming.Wizard.Api.Controllers;
         [HttpGet]
         public IActionResult GetItems()
         {
-            return Ok("Hello World!");
+            var items = new[]
+            {
+                new Item(name: "Item 1", description: "Description 1", brand: "Brand1", price: 100.00m),
+                new Item(name: "Item 2", description: "Description 2", brand: "Brand2", price: 200.00m),
+                new Item(name: "Item 3", description: "Description 3", brand: "Brand3", price: 300.00m),
+                new Item(name: "Item 4", description: "Description 4", brand: "Brand4", price: 400.00m),
+                new Item(name: "Item 5", description: "Description 5", brand: "Brand5", price: 500.00m),
+            };
+            return Ok(items);
         }
     }
--- src/api/Scheming.Wizard.Api/Program.cs
+++ src/api/Scheming.Wizard.Api/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Scheming Wizard API", Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Scheming Wizard API v1"));
 
 //app.MapGet("/", () => "Hello World!");
 app.MapControllers();
