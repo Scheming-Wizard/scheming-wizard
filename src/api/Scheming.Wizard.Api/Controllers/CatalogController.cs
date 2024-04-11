7db3fb31c7ab89404dab684427efbbff5a8da2f8
Tomosada Sato <Sato.77@buckeyemail.osu.edu>
2024-04-11 07:57:40 -0400

    Adding Post

--- src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
+++ src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
@@ -51,7 +51,9 @@ namespace Scheming.Wizard.Api.Controllers;
         [HttpPost]
         public IActionResult CreateItem(Item item)
         {
-            return CreatedAtAction(nameof(GetItem), new { id = 42 }, item);
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return Created($"api/catalot/{item.Id}", item);
         }
 
         [HttpPost("{id:int}/ratings")]
