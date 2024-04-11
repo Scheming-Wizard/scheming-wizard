af9f1fb55380266920e67be74d4fb7323c614d0e
Tomosada Sato <Sato.77@buckeyemail.osu.edu>
2024-04-11 08:35:27 -0400

    Adding Post Rating

--- src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
+++ src/api/Scheming.Wizard.Api/Controllers/CatalogController.cs
@@ -59,7 +59,14 @@ namespace Scheming.Wizard.Api.Controllers;
         [HttpPost("{id:int}/ratings")]
         public IActionResult AddRating(int id, Rating rating)
         {
-            return Ok();
+            var item = _context.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            item.AddRating(rating);
+            _context.SaveChanges();
+            return Ok(item);
         }
 
         [HttpPut("{id:int}")]
