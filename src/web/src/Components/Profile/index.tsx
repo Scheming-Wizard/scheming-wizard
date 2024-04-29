572bd67ebffd4e1f8e563af03fd827e14b42aa40
Ibrahim Naguthanawala <naguthanawala.2@osu.edu>
2024-04-28 20:00:03 -0400

    log in and out buttons

--- src/web/src/Components/Profile/index.tsx
+++ src/web/src/Components/Profile/index.tsx
@@ -1,27 +1,21 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-    const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-    if (isLoading) {
-        return <div>Loading ...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
-    if (!isAuthenticated) {
-        return null;
-    }
-
-    return (
-        isAuthenticated && (
-            <div className="flex items-center justify-center">
-                <img
-                    src={user?.picture}
-                    alt={user?.name}
-                    className="w-19 h-10 rounded-full"
-                />
-            </div>
-        )
-    );
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user?.picture} alt={user?.name} />
+        <h2>{user?.name}</h2>
+        <p>{user?.email}</p>
+      </div>
+    )
+  );
 };
 
 export default Profile;
